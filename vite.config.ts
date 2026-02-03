7bde01698ad2dd608276f77f4ef3c5640d9b2ebb
ICE <mbj0608@gmail.com>
2026-02-03 23:34:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    // 关键修改：添加下面这一行，因为你的仓库名叫 mimi
-    base: '/mimi/',
-    
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.')
-      }
-    }
-  };
+export default defineConfig({
+  // 必须加上这一行，mimi 是你的仓库名
+  base: '/mimi/',
+  plugins: [react()],
 });
-
